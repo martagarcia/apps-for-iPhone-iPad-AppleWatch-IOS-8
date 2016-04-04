545b0373ce8709a6b9aaa5672b7192d49cca23d7
martagarcia <mga1987@gmail.com>
2016-04-04 23:24:18 +0200

    Added files via upload

---
+++ 25Vista_Detalle_Apple_Watch/PetTableViewAppleWatch/PetTableViewAppleWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,51 @@
+//
+//  InterfaceController.swift
+//  PetTableViewAppleWatch WatchKit Extension
+//
+//  Created by Marta García on 23/3/15.
+//  Copyright (c) 2015 iOS8. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet weak var petTable: WKInterfaceTable!
+    
+    var petnames = ["Luna", "Dylan", "Mery", "Mady", "Paul Newman", "Heidi"]
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        petTable.setNumberOfRows(petnames.count, withRowType: "PetTableRowViewController")
+        
+        for (i,pet_name) in enumerate(petnames){
+            var rowpetcontroller = petTable.rowControllerAtIndex(i) as PetTableRowViewController
+            
+            rowpetcontroller.PetImage.setImage(UIImage(named: pet_name))
+            rowpetcontroller.PetLabel.setText(pet_name)
+        }
+        
+        // Configure interface objects here.
+    }
+    
+    override func contextForSegueWithIdentifier(segueIdentifier: String, inTable table: WKInterfaceTable, rowIndex: Int) -> AnyObject? {
+        
+        var petname = petnames[rowIndex]
+        
+        return petname
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
